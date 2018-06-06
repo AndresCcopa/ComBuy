5d81f2895c51902414994d9083d568553611a5cb
Raulalvaro <raulalvaro123@hotmail.com>
2018-06-06 00:24:04 -0500

    Add map fragment, bug localization

--- app/src/main/java/com/example/raul/combuyappv20/MainActivity.java
+++ app/src/main/java/com/example/raul/combuyappv20/MainActivity.java
@@ -1,85 +1,44 @@
 package com.example.raul.combuyappv20;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SearchProductFragment.OnFragmentInteractionListener {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
-        setSupportActionBar(toolbar);
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        //mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+        ToolbarFragment toolbarFragment = new ToolbarFragment();
+        /*
+        fragmentTransaction2.add(R.id.body_container,toolbarFragment);
+        fragmentTransaction2.commit();
+        */
+        fragmentTransaction2.add(R.id.layout_toolbar_container, toolbarFragment);
+        fragmentTransaction2.commit();
+
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-
-    //ACÁ VINCULA EL ACTION SETTINGS CON EL BOTON SETTINGS
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -87,17 +46,55 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        boolean FTransaction = false;
+        Fragment fragmentNavbar = null;
+        Fragment fragmentMap = null;
+
         if (id == R.id.nav_addubication) {
             // Handle the camera action
         } else if (id == R.id.nav_searchbusiness) {
+            fragmentNavbar = new SearchProductFragment();
+            fragmentMap = new MapPruebaFragment();
+            FTransaction = true;
 
         } else if (id == R.id.nav_searchproduct) {
 
         } else if (id == R.id.nav_settngs) {
 
         }
+
+        if(FTransaction){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+
+            fragmentTransaction.replace(R.id.layout_toolbar_container,fragmentNavbar);
+            fragmentTransaction.replace(R.id.layout_map_container,fragmentMap).commit();
+            fragmentTransaction.addToBackStack(null);
+            /*
+            FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.layout_map_container, fragmentNavbar).commit();
+            */
+
+
+            /*
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.map_container,fragmentMap).commit();
+
+            FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.layout_body_container, fragmentNavbar).commit();
+
+            fragmentTransaction.addToBackStack(null);
+            */
+
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/raul/combuyappv20/MapPruebaFragment.java
@@ -0,0 +1,273 @@
+package com.example.raul.combuyappv20;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.raul.combuyappv20.utils.CombuyLocal;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import static com.example.raul.combuyappv20.utils.CombuyUtils.obtenerCercanos;
+import static com.example.raul.combuyappv20.utils.CombuyUtils.test;
+
+public class MapPruebaFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback{
+
+    private OnFragmentInteractionListener mListener;
+
+    //Variables de mapas
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private GoogleMap mMap;
+    private Location CurrentLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int permiso = 0;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationrequest;
+    private boolean PermisoConcedido ;
+    private MapView mapView;
+
+
+    private List<CombuyLocal> locales;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    public MapPruebaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_map_prueba, container, false);
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.v("COSOSOSOOSS", "String");
+                mMap = googleMap;
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+                //mMap.setMyLocationEnabled(true);
+
+                agregarMarcador(-33, 152, "XD", "Hola");
+
+
+                Log.v("COSOSOSOOSS", "COSO 2");
+                ObtenerPermisodeUbicacion();
+                Log.v("COSOSOSOOSS", "COSO 3");
+                updateLocationUI();
+                Log.v("COSOSOSOOSS", "COSO 4");
+                ObtenerUbicacion(); // Obtiene Ubicacion del dispositivo y coloca la posicion en el mapa
+                Log.v("COSOSOSOOSS", "COSO 5");
+                //obtenerLocales();
+
+            }
+        });
+
+        return view;
+    }
+
+
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+    //Metodos del MAPA
+
+    private void agregarMarcador(double Lat,double Lng,String nombre,String descripcion){
+        Log.v("RETRO","AGREGANDO MARCADORES");
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(Lat, Lng))
+                .title(nombre)
+                .snippet(descripcion));
+
+    }
+
+    private void obtenerLocales() {
+        Log.v("MAPS","OBTENIENDO LOCALES");
+        if(locales!=null){
+            for(CombuyLocal i : locales){
+                agregarMarcador(i.getLatitud(),i.getLongitud(),i.getNombrenegocio(),i.getDescripcion());
+            }
+        }else{
+            Toast.makeText(getContext(),"No se encontro ningun local u.u", Toast.LENGTH_LONG).show();
+
+        }
+
+
+
+    }
+
+    private void ObtenerPermisodeUbicacion() {
+        /*
+         * Consulta el permison de FINE_LOCATION, el resultado es manejado por el callback
+         * onRequestPermissionsResult
+         */
+        if (ContextCompat.checkSelfPermission(this.getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            PermisoConcedido = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        PermisoConcedido = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    PermisoConcedido = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (PermisoConcedido) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                CurrentLocation = null;
+                ObtenerPermisodeUbicacion();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void ObtenerUbicacion() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (PermisoConcedido) {
+                Task<Location> locationResult = mFusedLocationClient.getLastLocation();
+
+                locationResult.addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            CurrentLocation = task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(CurrentLocation.getLatitude(),
+                                            CurrentLocation.getLongitude()), DEFAULT_ZOOM));
+                            Log.v("TASK","ESTA ES LA UBICACION");
+                            Log.v("TASK",test(CurrentLocation));
+
+                            locales = obtenerCercanos(CurrentLocation,5);
+                            obtenerLocales();
+
+                            //locales=CombuyUtils.obtenerCercanos(CurrentLocation,2);
+                        } else {
+                            Log.d("MAPS", "Current location is null. Using defaults.");
+                            Log.e("MAPS", "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
